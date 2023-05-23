f2ab4d9ee725fb45711773eae1ea76e87ba5e9b2
KhokonPHB07 <kshehala@gmail.com>
2023-05-24 01:36:34 +0600

    home page divided 3 section

--- src/Pages/Recipie/Recipie.jsx
+++ src/Pages/Recipie/Recipie.jsx
@@ -12,7 +12,7 @@ const Recipie = () => {
         <div>
             <Header></Header>
             <Navbar></Navbar>
-        <h1 className='text-lg font-semibold italic text-center'>Here is our all Famous Chef</h1>
+        <h1 className='text-lg font-semibold italic text-center mt-10'>-------------------- Here is our all Famous Chef --------------------</h1>
             <div className='grid grid-cols-3 mx-8 my-20 gap-8'>
                 {
                     chefDetails.map(chef => <ChefsBio
@@ -23,14 +23,19 @@ const Recipie = () => {
                 }
             </div>
             {/* this is 2nd section start */}
-            <h1 className='text-lg font-semibold italic text-center'>Customize and order your food</h1>
+            <h1 className='text-lg font-semibold italic text-center'>-------------------- Customize and order your food --------------------</h1>
             <div>
                 <div className="hero min-h-screen bg-base-200 mt-10 mb-10">
                     <div className="hero-content flex-col lg:flex-row">
-                        <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                        <img src="https://i.ibb.co/55n9TC5/02.jpg" className="max-w-sm rounded-lg shadow-2xl" />
                         <div>
-                            <h1 className="text-5xl font-bold">Box Office News!</h1>
-                            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                            <h1 className="text-5xl font-bold">Customize and order your food</h1>
+                            <p className="py-6">If you are spicy lover choose our all lovely spice item</p>
+                            <li>Hot rice with chili chicken</li>
+                            <li>Hot beef seezling</li>
+                            <li>Mashroom with Onion</li>
+                            <li>Ilish fish with onion fry</li>
+                            <li>Dry fish with chili fleaks</li>
                             <button className="btn btn-primary">Get Started</button>
                         </div>
                     </div>
@@ -39,14 +44,14 @@ const Recipie = () => {
             {/* this is 2nd section end */}
             
             {/* this is 3rd section start */}
-            <h1 className='text-lg font-semibold italic text-center'>Choose your Menu</h1>
+            <h1 className='text-lg font-semibold italic text-center mb-10'>-------------------- Choose your Menu --------------------</h1>
             <div>
                 <div className="hero min-h-screen bg-base-200 mb-10">
                     <div className="hero-content flex-col lg:flex-row">
-                        <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                        <img src="https://i.ibb.co/YRPjGw2/03.jpg" className="max-w-sm rounded-lg shadow-2xl" />
                         <div>
-                            <h1 className="text-5xl font-bold">Box Office News!</h1>
-                            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                            <h1 className="text-5xl font-bold">Choose your Menu</h1>
+                            <p className="py-6">From our 100+ Item select your love one</p>
                             <button className="btn btn-primary">Get Started</button>
                         </div>
                     </div>
